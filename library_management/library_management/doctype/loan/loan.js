d1e9dff72efe45497366310e2d40953cb1e2b5be
MichaelAngelo-11 <p.rucakibun@alustudent.com>
2026-02-18 15:11:10 +0000

    this week session

--- library_management/library_management/doctype/loan/loan.js
+++ library_management/library_management/doctype/loan/loan.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2026, Michael  and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Loan", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Loan", {
+	refresh(frm) {
+        // frm.set_query("field_name", function())
+        frm.set_query("book",function(){
+            return{
+                //filters:{status:"Available"}
+                filters:{status:["!=","Available"]}
+            }
+        })
+	},
+});
